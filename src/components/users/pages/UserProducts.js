1da106418f1c202f1eb9da211e03d4ca35dc97c0
FACTS  LIMITED <admin@factsafrica.com>
2020-08-14 11:59:54 +0300

    use custom http hook to fetch user products

--- src/components/users/pages/UserProducts.js
+++ src/components/users/pages/UserProducts.js
@@ -1,24 +1,21 @@
 import React, { useEffect, useReducer, useCallback, useContext } from 'react';
 import { useParams, NavLink } from 'react-router-dom';
 import AliceCarousel from 'react-alice-carousel';
+import { CircularProgress } from '@material-ui/core';
+import { useHttp } from '../../../shared/http-hook'
 import { AuthContext } from '../../../shared/auth-context'
 import Card from '../../UI/Card/card';
+import ErrorModal from '../../UI/ErrorModal';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 const initialState = {
-  userProducts: [],
-  isLoading: false,
-  error: null,
+  userProducts: []
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_PRODUCTS':
-      return { ...state, userProducts: action.products, isLoading: false };
-    case 'FETCH_PRODUCTS':
-      return { ...state, isLoading: true };
-    case 'FETCH_PRODUCTS_FAIL':
-      return { ...state, error: action.error };
+      return { ...state, userProducts: action.products };
     default:
       return state;
   }
@@ -27,36 +24,32 @@ const reducer = (state, action) => {
 const UserProducts = () => {
   const id = useParams();
   const [productState, dispatch] = useReducer(reducer, initialState);
+  const { isLoading, error, sendRequest, clearError } = useHttp()
   const auth = useContext(AuthContext)
 
   const fetchUserProducts = useCallback(async () => {
-    dispatch({ type: 'FETCH_PRODUCTS' });
-    try {
-      const response = await fetch(
-        `http://localhost:5000/api/products/user/${id.id}`
-      );
-      const resData = await response.json();
-      if (!response.ok) {
-        throw new Error('Could not fetch products');
-      }
+    const url = `http://localhost:5000/api/products/user/${id.id}`
+    try{
+      const resData = await sendRequest(url)
       dispatch({ type: 'SET_PRODUCTS', products: resData.products });
     } catch (error) {
-      dispatch({ type: 'FETCH_PRODUCTS_FAIL', error: error.message });
+      
     }
-  }, [id]);
+  }, [id, sendRequest]);
 
   useEffect(() => {
     fetchUserProducts();
   }, [fetchUserProducts]);
 
-  return productState.userProducts.map((prod) => {
+  return error? <ErrorModal error={error} onClear={clearError}/> :
+  productState.userProducts.map((prod) => {
     return (
       <Card className="authentication" key={prod.id}>
-        <AliceCarousel autoPlay autoPlayInterval={3000}>
+        {isLoading? <CircularProgress /> : <AliceCarousel autoPlay autoPlayInterval={3000}>
           <img src={prod.imageUrls.angleOne} className="sliderimg" alt="" />
           <img src={prod.imageUrls.angleOne} className="sliderimg" alt="" />
           <img src={prod.imageUrls.angleOne} className="sliderimg" alt="" />
-        </AliceCarousel>
+        </AliceCarousel>}
         <div>
           <h2>{prod.name}</h2>
           <h4 style={{ color: 'blue', fontWeight: 'bold' }}>
