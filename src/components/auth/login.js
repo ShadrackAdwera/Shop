f697a43142af11016f95fe775bb558bd1d7d9a57
FACTS  LIMITED <admin@factsafrica.com>
2020-08-14 11:34:47 +0300

    use the custom http hook on the login component

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useContext } from 'react';
+import React, { useReducer, useContext } from 'react';
 import { useHistory } from 'react-router-dom'
 import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
@@ -8,6 +8,7 @@ import { CircularProgress } from '@material-ui/core';
 import { AuthContext } from '../../shared/auth-context'
 import ErrorModal from '../UI/ErrorModal';
 import Card from '../UI/Card/card';
+import { useHttp } from '../../shared/http-hook'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -41,39 +42,25 @@ const LogIn = () => {
   const classes = useStyles();
   const auth = useContext(AuthContext)
   const [formState, dispatch] = useReducer(loginReducer, initialState);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, clearError, sendRequest } = useHttp()
   const history = useHistory()
 
   const logIn = async () => {
+    const url = 'http://localhost:5000/api/users/login'
     const userInfo = { email: formState.email, password: formState.password };
-    setIsLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userInfo),
-      });
-      const resData = await response.json();
-      setIsLoading(false)
-      if (!response.ok) {
-          setIsLoading(false);
-          throw new Error(resData.error);
-      } else {
-        auth.login()
-        history.push('/')
-      }
+      await sendRequest(url, 'POST', JSON.stringify(userInfo), {'Content-Type':'application/json'})
+      auth.login()
+      history.push('/')
     } catch (error) {
-      setError(error.message);
-      setIsLoading(false);
+      console.log(error)
     }
+    
   };
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={() => setError(null)} />
+      <ErrorModal error={error} onClear={clearError} />
       <Card className="authentication">
         <div className={classes.root}>
           <h2>
