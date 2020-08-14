f1844244d9d769754498cde480f42d935a29c1c5
FACTS  LIMITED <admin@factsafrica.com>
2020-08-14 11:49:30 +0300

    use the custom http hook to fetch users

--- src/components/users/pages/Users.js
+++ src/components/users/pages/Users.js
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import UserList from '../components/UserList'
-import { CircularProgress } from '@material-ui/core'
-import ErrorModal from '../../UI/ErrorModal'
+import React, { useEffect, useState, useCallback } from 'react';
+import { CircularProgress } from '@material-ui/core';
+import { useHttp } from '../../../shared/http-hook';
+import UserList from '../components/UserList';
+import ErrorModal from '../../UI/ErrorModal';
 
 const Users = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState()
-    const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const { isLoading, error, sendRequest, clearError } = useHttp();
 
-    useEffect(()=>{
-        fetchUsers()
-    }, [])
+  const fetchUsers = useCallback(async () => {
+    const url = 'http://localhost:5000/api/users';
+    try {
+      const resData = await sendRequest(url);
+      setUsers(resData.users);
+    } catch (error) {}
+  }, [sendRequest]);
 
-    const fetchUsers = async () => {
-        setIsLoading(true)
-        try {
-            const response = await fetch('http://localhost:5000/api/users')
-        const resData = await response.json()
-        if(!response.ok) {
-            throw new Error('Could not find users')
-        }
-        setUsers(resData.users)
-        setIsLoading(false)
-        } catch (error) {
-            setIsLoading(false)
-            setError('Could not find users')
-        }
-    }
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
- return <React.Fragment>
-     <ErrorModal error={error} onClear={()=>setError(null)}/>
-     {isLoading? <div className='center'><CircularProgress/></div>: <UserList users={users}/>}
- </React.Fragment>
-}
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading ? (
+        <div className="center">
+          <CircularProgress />
+        </div>
+      ) : (
+        <UserList users={users} />
+      )}
+    </React.Fragment>
+  );
+};
 
-export default Users
+export default Users;
